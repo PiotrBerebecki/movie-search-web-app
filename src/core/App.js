372e08993f6810cdb165bb7fe74ecbaa7380ec32
PiotrBerebecki <petebere@gmail.com>
2017-06-09 00:28:21 +0100

    Setup folder structure

--- src/core/App.js
+++ src/core/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import SearchResults from './../views/SearchResults';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Movie Search App</h1>
+        <SearchResults />
       </div>
     );
   }
---
+++ src/views/SearchResults/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SearchResults extends Component {
+  render() {
+    return <p>Search Results</p>;
+  }
+}
+
+export default SearchResults;
