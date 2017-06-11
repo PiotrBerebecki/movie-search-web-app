987a742f0a676d35e332a4194bc508b5ec7947b8
PiotrBerebecki <petebere@gmail.com>
2017-06-12 00:25:15 +0100

    Include test summary

--- src/helpers/addThreeDots.test.js
+++ src/helpers/addThreeDots.test.js
@@ -1,7 +1,7 @@
 import addThreeDots from './addThreeDots.js';
 
 describe('addThreeDots', () => {
-  it('', () => {
+  it('adds three dots to the end of a string', () => {
     const input = 'Welcome to the desert of the real';
     const expected = 'Welcome to the desert of the real...';
     expect(addThreeDots(input)).toEqual(expected);
