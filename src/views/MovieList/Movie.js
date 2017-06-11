248cf5ed54f6658b2e0b357382ec88a03e1b7ee3
PiotrBerebecki <petebere@gmail.com>
2017-06-11 23:50:12 +0100

    Trim movie summaries and and three dots after a full word

--- src/views/MovieList/Movie.js
+++ src/views/MovieList/Movie.js
@@ -8,6 +8,8 @@ import {
   MovieOverview,
 } from './../../styles';
 import trimTextToFullWord from './../../helpers/trimTextToFullWord';
+import addThreeDots from './../../helpers/addThreeDots';
+import pipe from './../../helpers/pipe';
 
 const baseUrlCover = 'https://image.tmdb.org/t/p/w500';
 
@@ -17,7 +19,9 @@ function Movie({ cover, title, summary }) {
       <MovieImage url={`${baseUrlCover}${cover}`} />
       <MovieInfo>
         <MovieTitle>{title}</MovieTitle>
-        <MovieOverview>{trimTextToFullWord(summary, 0, 100)}</MovieOverview>
+        <MovieOverview>
+          {pipe(trimTextToFullWord, addThreeDots)(summary, 0, 100)}
+        </MovieOverview>
       </MovieInfo>
     </MovieCard>
   );
