168765ddfef9b59a5d03017a303ff8470558236c
PiotrBerebecki <petebere@gmail.com>
2017-06-12 13:38:44 +0100

    Add comments explaining the purpose of the url constructing functions

--- src/constants/movieDb.js
+++ src/constants/movieDb.js
@@ -3,6 +3,12 @@ export const baseUrlCover = 'https://image.tmdb.org/t/p/w500';
 const baseUrlSearch = 'https://api.themoviedb.org/3/search/movie?api_key=';
 const searchUrlOptions = '&query=';
 
+/**
+ * Constructs url for searching movie database
+ * @param  {string} apiKey     api key
+ * @param  {string} searchText movie title
+ * @return {string}            movie db url
+ */
 export const getSearchUrl = (apiKey, searchText) => {
   return `${baseUrlSearch}${apiKey}${searchUrlOptions}${encodeURIComponent(searchText)}`;
 };
@@ -10,6 +16,11 @@ export const getSearchUrl = (apiKey, searchText) => {
 const baseUrlDiscover = 'https://api.themoviedb.org/3/discover/movie?api_key=';
 const discoverUrlOptions = '&sort_by=popularity.desc&with_genres=878';
 
+/**
+ * Constructs url for discovering movies in the database.
+ * @param  {string} apiKey api key
+ * @return {string}        movie db url
+ */
 export const getDiscoverUrl = apiKey => {
   return `${baseUrlDiscover}${apiKey}${discoverUrlOptions}`;
 };
